25adbc81b930bd3f8fbe8d6d8adadbe166e8d18a
Niko Fotopoulos <fottopoulos@gmail.com>
2023-11-20 17:33:19 +0100

    Add responsive settings

--- app/myAccount/page.tsx
+++ app/myAccount/page.tsx
@@ -3,9 +3,9 @@ import SignUpForm from './Components/SignUpForm';
 
 export default function MyAccount() {
   return (
-    <div className="pt-10 pb-10 pl-10 pr-10 md:pl-0 md:pr-0 flex flex-col md:flex-row items-center justify-center">
+    <div className="p-10 md:grid-cols-3md:gap-1 md:pl-10 md:pr-0 flex flex-col md:flex-row items-center justify-center">
       <SignInForm />
-      <div className="h-[200px] md:w-2 bg-gray-300 my-4 md:my-0 md:mx-16" />
+      <hr className="h-[200px] md:w-2 bg-gray-700 rounded-lg my-4 md:my-0 md:mx-16" />
       <SignUpForm />
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 export default function Home() {
   return (
-    <div className="pl-10 pt-10 pb-36 grid grid-cols-1 md:grid-cols-3 gap-1 indent-10 leading-6	 text-3xl">
-      <div className="font-display text-indigo-800 font-[900]  tracking-wider">
+    <div className="pl-10 pt-10 pb-36 grid gap-1 indent-10 leading-6 text-3xl lg:grid-cols-3 md:gap-1">
+      <div className="font-display text-indigo-800 font-[900] tracking-wider">
         INDEPENDENT SPACE INDEX IS A COMPREHENSIVE DIRECTORY OF ARTIST-RUN,
         NON-COMMERCIAL, AND INDEPENDENT SPACES FOR CONTEMPORARY ART (SOMETIMES
         CALLED “OFF-SPACES” OR ALTERNATIVE SPACES IN VIENNA) AS WELL AS A
         NETWORKING CHANNEL BETWEEN THE LISTED SPACES.
       </div>
-      <div className="font-display text-cyan-800 font-[900]  tracking-wider">
+      <div className="font-display text-cyan-800 font-[900] tracking-wider">
         INDEPENDENT SPACES, THE NETWORK WAS FORMED IN 2017 WITH THE AIM OF
         STRENGTHENING SOLIDARITY AMONG LIKE-MINDED CULTURAL INITIATIVES,
         INCREASING THE VISIBILITY OF INDEPENDENT ARTISTIC PRODUCTION BY
         ARCHIVING ITS LOCALES AND FACILITATING THEIR DISCOVERY, AND HIGHLIGHTING
         THEIR CONTRIBUTION TO THE VIENNESE ART SCENE.
       </div>
-      <div className="font-display text-purple-800 font-[900]  tracking-wider">
+      <div className="font-display text-purple-800 font-[900] tracking-wider">
         THE LIST OF SPACES IS REGULARLY MAINTAINED. THERE ARE CURRENTLY 87
         ACTIVE SPACES LISTED.
       </div>
